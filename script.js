a7e198910725673032d1702381f53a6987836295
Nicola <nicolamelito.11@gmail.com>
2020-10-20 17:48:51 +0200

    modificato prezzario
    come da nota di Alessandro

--- script.js
+++ script.js
@@ -2,20 +2,20 @@ var km = parseInt(prompt("Quanti Km percorriamo?"));
 
 var age = parseInt(prompt("Quanti anni hai?"));
 
-var price = 0.21;
+var unitPrice = 0.21;
 
 
 
 if (!isNaN(km) && !isNaN(age)) {
 
 if (age <= 18) {
-  price = km * 0.8;
+  price = km * unitPrice * 0.8;
   console.log(price);
 } else if (age >= 65) {
-  price = km * 0.6;
+  price = km * unitPrice * 0.6;
   console.log(price);
 } else {
-  price = km * 0.21;
+  price = km * unitPrice;
   console.log(price);
  }
 }
