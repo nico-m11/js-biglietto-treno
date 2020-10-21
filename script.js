dcd9f478f880a667978a4c624fea503e3f9bac2e
Nicola <nicolamelito.11@gmail.com>
2020-10-21 15:31:35 +0200

    Prime modifiche alla pagine
    Inizio creazione dell HTML e del CSS parte 1

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-var km = parseInt(prompt("Quanti Km percorriamo?"));
+//var km = parseInt(prompt("Quanti Km percorriamo?"));
 
-var age = parseInt(prompt("Quanti anni hai?"));
+//var age = parseInt(prompt("Quanti anni hai?"));
 
-var unitPrice = 0.21;
+//var unitPrice = 0.21;
 
 
 
-if (!isNaN(km) && !isNaN(age)) {
+//if (!isNaN(km) && !isNaN(age)) {
 
-if (age <= 18) {
-  price = km * unitPrice * 0.8;
-  console.log(price);
-} else if (age >= 65) {
-  price = km * unitPrice * 0.6;
-  console.log(price);
-} else {
-  price = km * unitPrice;
-  console.log(price);
- }
-}
+//if (age <= 18) {
+//  price = km * unitPrice * 0.8;
+  //console.log(price);
+//} else if (age >= 65) {
+//  price = km * unitPrice * 0.6;
+//  console.log(price);
+//} else {
+//  price = km * unitPrice;
+//  console.log(price);
+// }
+//}
 
-document.getElementById("risult").innerHTML = ("Il costo del tuo boglietto Treno è di €: " + price.toFixed(2));
+//document.getElementById("risult").innerHTML = ("Il costo del tuo boglietto Treno è di €: " + price.toFixed(2));
