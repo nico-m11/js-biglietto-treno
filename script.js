71500e8d2402f71029a2dc38673faea0b6c4c758
Nicola <nicolamelito.11@gmail.com>
2020-10-21 17:01:38 +0200

    seconda parte

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-//var km = parseInt(prompt("Quanti Km percorriamo?"));
 
-//var age = parseInt(prompt("Quanti anni hai?"));
 
-//var unitPrice = 0.21;
+
 
 
 
@@ -21,3 +19,29 @@
 //}
 
 //document.getElementById("risult").innerHTML = ("Il costo del tuo boglietto Treno è di €: " + price.toFixed(2));
+
+var btnG = document.getElementById('btnG');
+
+btnG.addEventListener("click", function () {
+  var nome = document.getElementById("name");
+  var distanza = document.getElementById("km");
+  var ageDiscount = document.getElementById("ageDiscount");
+  var unitPrice = 0.21;
+
+
+ if (!isNaN(km) && !isNaN(age)) {
+  if (optionOne) {
+    price = km * unitPrice * 0.8;
+    console.log(price);
+} else if (optionTwo) {
+    price = km * unitPrice * 0.6;
+    console.log(price);
+} else  {
+    price = km * unitPrice;
+    console.log(price);
+}
+}
+document.getElementById("offerta").innerHTML = ("sconto applicato" + ageDiscount);
+document.getElementById("random").innerHTML = Math.floor(Math.random() * 10);
+document.getElementById("randomDue").innerHTML = Math.floor(Math.random() * 100);document.getElementById("total").innerHTML = ("€" + price);
+})
